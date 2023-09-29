af3c98b858ca7f20eed33dc9b22b4dd3b7f8a0aa
kociotrzeci <80517383+kociotrzeci@users.noreply.github.com>
2023-09-29 08:10:45 +0200

    this one was easy

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    if (number1<0 || number2<0) return "ERROR";
+    if (typeof(number1)!="number" ||typeof(number2)!="number") return "ERROR";
+    if (number1>number2)
+    {
+        const temp = number2;
+        number2 = number1;
+        number1 = temp;
+    }
+    let sum=0;
+    for (let i = number1; i<=number2; i++)
+        {
+            sum = sum+i
+        }
+    return sum;
 };
 
 // Do not edit below this line
